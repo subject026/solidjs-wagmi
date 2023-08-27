cd62974be2d9120a65ce11ea62ba853207e7838a
subject026 <subject026@protonmail.com>
2023-08-27 23:10:46 +0100

    toggle named chunks

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,21 +6,21 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs(), tailwind()],
-  vite: {
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks(id) {
-            if (id.includes("wagmi") || id.includes("viem")) {
-              return "wagmi";
-            }
-            if (id.includes("d3")) {
-              return "d3";
-            }
-            return null;
-          },
-        },
-      },
-    },
-  },
+  // vite: {
+  //   build: {
+  //     rollupOptions: {
+  //       output: {
+  //         manualChunks(id) {
+  //           if (id.includes("wagmi") || id.includes("viem")) {
+  //             return "wagmi";
+  //           }
+  //           if (id.includes("d3")) {
+  //             return "d3";
+  //           }
+  //           return null;
+  //         },
+  //       },
+  //     },
+  //   },
+  // },
 });
